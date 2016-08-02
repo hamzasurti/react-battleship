52fc9304ad6fec03974b4109327719466c6e11a5
hamzasurti <hamza.surti1@gmail.com>
2016-08-01 19:43:59 -0700

    added production server

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
-const config = require('./webpack.config');
+const config = require('./webpack.dev.config');
+const path = require('path');
+const express = require('express');
 
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true,
-}).listen(3000, 'localhost', (err) => {
-  if (err) {
-    return console.log(err);
-  }
+if (process.env.NODE_ENV !== 'production') {
+  new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true,
+  }).listen(3000, 'localhost', (err) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log('Listening at http://localhost:3000/');
+  });
+} else {
+  const app = express();
+  const indexPath = path.join(__dirname, './index.html');
+  const publicPath = express.static(path.join(__dirname, './client'));
 
-  console.log('Listening at http://localhost:3000/');
-});
+  app.use('/public', publicPath);
+  app.get('/', function (_, res) { res.sendFile(indexPath); });
+  app.listen(3000);
+  console.log('Listening at port 3000');
+}
+
--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,6 @@ import BattleShipApp from '../containers/BattleShipApp';
 import AvailableShips from '../containers/AvailableShips';
 // import EnemyBoard from '../containers/EnemyBoard';
 
-
-
 const App = () => (
   <div>
     <h1>Battleship</h1>
